47ee5d0dff1c8725e2c203589a43acc52e59dff4
AB.Nikitin <AB.Nikitin@bk.ru>
2015-02-16 22:30:11 +0400

    JSP is VIEW

--- src/main/java/abc/def/web/controller/RootController.java
+++ src/main/java/abc/def/web/controller/RootController.java
@@ -12,9 +12,14 @@
  */
 package abc.def.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author annik
@@ -30,4 +35,13 @@ public class RootController {
         return "hello";
     }
 
+    @RequestMapping( value = "/test.htm" )
+    @ResponseBody
+    public ModelAndView testUrl( HttpServletRequest request, HttpServletResponse response ) {
+
+        ModelAndView model = new ModelAndView( "test" );
+        model.addObject( "msg", "Controller Interface Example!" );
+        
+        return model;
+    }
 }
